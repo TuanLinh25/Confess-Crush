b60b659457577512007174b4adb9556ad6bbdf67
Tuấn Anh Phạm Văn <95533619+TuanLinh25@users.noreply.github.com>
2021-12-04 20:46:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Người Tôi Yêu",
-    introTitle: 'Họ tên người yêu',
+    introTitle: 'Họ và tên người yêu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
